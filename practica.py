22f75bec02987e464d095b911acd20affeae0092
Sergio <sergio.marcos@campusfp.es>
2022-09-30 13:51:39 +0200

    ejercicio practica

---
+++ practica.py
@@ -0,0 +1,43 @@
+numero=5
+string="numero"
+
+print("El "+string +" es "+str(numero))
+
+decimal=5.3
+entero=8
+
+print(entero-decimal)
+
+
+
+#array
+
+texto="En una lugar de la Mancha"
+
+print(texto)
+print(texto[3])
+
+print(texto[2:8]) #va desde el caracter dos hasta el siete
+
+
+
+
+#asignar variables
+
+Nombre = "Sergio"
+Edad=5.4
+
+print("hola,soy {} y tengo {} años".format(Nombre,Edad))
+
+
+#meter datos
+
+"""entrada= input()
+
+print(entrada)"""
+
+
+
+
+
+
